b8f7ef75bf015cc75419b5c23d1870cda66348d2
Edinunzio <e.dinunzio@gmail.com>
2014-04-04 15:56:22 -0400

    created django project

---
+++ assets/js/read.js
@@ -0,0 +1,18 @@
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+readTextFile("/home/elizabeth/Projects/rtg/rtg/assets/tao.txt");
